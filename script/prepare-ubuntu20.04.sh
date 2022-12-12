8c18d9a649205965ba36baf09c326ba7e727f543
LucFabresse <luc.fabresse@gmail.com>
2022-12-12 15:23:25 +0100

    add update chrome key at install

--- script/prepare-ubuntu20.04.sh
+++ script/prepare-ubuntu20.04.sh
@@ -5,6 +5,7 @@ sudo cp config/u20-sources.list /etc/apt/sources.list
 
 echo "# Dependencies"
 echo "#----------------------------"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4EB27DB2A3B88B8B # update expired chrome key
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git git-lfs openssh-server sshfs \
